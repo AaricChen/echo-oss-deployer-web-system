94b2bb65f2abd357e71f61acdd99199d69a886c9
Aaric <aaricchen@gmail.com>
2025-07-09 22:06:26 +0800

    Refactor login page and enhance background handling
    - Replaced LoginFormPage with LoginForm for improved structure and functionality.
    - Updated the layout of the login page to include a dynamic background image from appConfig.
    - Streamlined the form fields for username and password input, maintaining validation rules.

--- app/routes/_auth.login.tsx
+++ app/routes/_auth.login.tsx
@@ -1,6 +1,6 @@
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import {
-  LoginFormPage,
+  LoginForm,
   ProFormSelect,
   ProFormText,
 } from "@ant-design/pro-components";
@@ -13,69 +13,66 @@ export default function LoginPage() {
   const { token } = theme.useToken();
   const login = useLogin();
   return (
-    <LoginFormPage<LoginRequest>
-      logo={appConfig.logo}
-      backgroundImageUrl={appConfig.loginBackground}
-      title="欢迎"
-      subTitle="登录到你的账户"
-      containerStyle={{
-        backgroundColor: "rgba(0, 0, 0,0.65)",
-        backdropFilter: "blur(4px)",
-      }}
-      onFinish={async (values) => {
-        await login.mutateAsync(values);
-      }}
-    >
-      <ProFormSelect
-        name="type"
-        hidden
-        initialValue={AuthenticationType.USERNAME}
-        options={Object.values(AuthenticationType).map((type) => ({
-          label: type,
-          value: type,
-        }))}
-      />
-      <ProFormText
-        name="identity"
-        fieldProps={{
-          size: "large",
-          prefix: (
-            <UserOutlined
-              style={{
-                color: token.colorText,
-              }}
-            />
-          ),
+    <div style={{ backgroundColor: token.colorBgContainer }}>
+      <LoginForm<LoginRequest>
+        logo={appConfig.logo}
+        title="欢迎"
+        subTitle="登录到你的账户"
+        onFinish={async (values) => {
+          await login.mutateAsync(values);
         }}
-        placeholder={"请输入用户名"}
-        rules={[
-          {
-            required: true,
-            message: "请输入用户名!",
-          },
-        ]}
-      />
-      <ProFormText.Password
-        name="credential"
-        fieldProps={{
-          size: "large",
-          prefix: (
-            <LockOutlined
-              style={{
-                color: token.colorText,
-              }}
-            />
-          ),
-        }}
-        placeholder={"请输入密码"}
-        rules={[
-          {
-            required: true,
-            message: "请输入密码!",
-          },
-        ]}
-      />
-    </LoginFormPage>
+      >
+        <ProFormSelect
+          name="type"
+          hidden
+          initialValue={AuthenticationType.USERNAME}
+          options={Object.values(AuthenticationType).map((type) => ({
+            label: type,
+            value: type,
+          }))}
+        />
+        <ProFormText
+          name="identity"
+          fieldProps={{
+            size: "large",
+            prefix: (
+              <UserOutlined
+                style={{
+                  color: token.colorText,
+                }}
+              />
+            ),
+          }}
+          placeholder={"请输入用户名"}
+          rules={[
+            {
+              required: true,
+              message: "请输入用户名!",
+            },
+          ]}
+        />
+        <ProFormText.Password
+          name="credential"
+          fieldProps={{
+            size: "large",
+            prefix: (
+              <LockOutlined
+                style={{
+                  color: token.colorText,
+                }}
+              />
+            ),
+          }}
+          placeholder={"请输入密码"}
+          rules={[
+            {
+              required: true,
+              message: "请输入密码!",
+            },
+          ]}
+        />
+      </LoginForm>
+    </div>
   );
 }
 
--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -1,10 +1,19 @@
 import { ProConfigProvider } from "@ant-design/pro-components";
 import { Outlet } from "react-router";
+import { appConfig } from "~/configs/app";
 
 export default function Auth() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-white">
-      <ProConfigProvider dark>
+    <div
+      className="flex h-screen w-screen flex-col items-center justify-center bg-white"
+      style={{
+        backgroundImage: `url(${appConfig.loginBackground})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <ProConfigProvider>
         <Outlet />
       </ProConfigProvider>
     </div>
